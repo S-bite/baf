2a4094ba3f5887aa48f05c48ea41837d7722d897
S-bite <0125bitbite@gmail.com>
2020-12-12 23:45:03 +0900

    fix bug in generate_every_possible_code

--- generator.py
+++ generator.py
@@ -6,7 +6,10 @@ def generate_every_possible_code(choosable_tokens):
     for choosable_token in choosable_tokens:
         updated_possible_codes = []
         for possible_code in possible_codes:
-            for token in choosable_token:
-                updated_possible_codes.append(possible_code + token)
+            if isinstance(choosable_token, list):
+                for token in choosable_token:
+                    updated_possible_codes.append(possible_code + token)
+            else:
+                updated_possible_codes.append(possible_code + choosable_token)
         possible_codes = updated_possible_codes
     return possible_codes
